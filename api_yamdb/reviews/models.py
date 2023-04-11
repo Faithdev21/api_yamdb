72b042cc40bc59a70087ad66f76cd4f2a8fe6b45
Егор Лоскутов <egorkaloskutov@yandex.ru>
2023-04-11 16:09:27 +0300

    Issue 6: Create model, view, endpoint for categories

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=50)
+
+    def __str__(self):
+        return self.name
