fedc3b5fcb84e2bbbb604e42fc4138a1281a7f69
Иван Березный <iberenznyi@yandex.ru>
2023-04-16 02:34:06 +0300

    Issues 9: model_feedback_is_done_mark3

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Feedback(models.Model):
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='feedback',
+        verbose_name='product'
+    )
+    text = models.CharField(
+        max_length=200
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='feedback',
+        verbose_name='author'
+    )
+    pub_date = models.DateTimeField(
+        'publication date',
+        auto_now_add=True,
+        db_index=True
+    )
+
+    class Meta:
+        verbose_name = 'feedback'
+        verbose_name_plural = 'feedbacks'
+        constraints = [
+            models.UniqueConstraint(
+                fields=('title', 'author', ),
+                name='unique feedback'
+            )]
+        ordering = ('pub_date',)
+
+    def __str__(self):
+        return self.text
