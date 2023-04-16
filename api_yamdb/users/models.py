d2b6b481e031ed80ab434097bed60f58df0d72c1
Павел Пономарев <permanganatoff@gmail.com>
2023-04-16 15:11:03 +0400

    Fixed user model

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,54 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.core.validators import RegexValidator
 
 
-ROLE_CHOICES = (
+ROLE_CHOICES = [
     ('USER', 'user'),
     ('MODERATOR', 'moderator'),
     ('ADMIN', 'admin'),
-)
+]
 
 
 class User(AbstractUser):
-    bio = models.TextField(
-        'Биография',
-        blank=True,
-    )
-    username = models.CharField(
-        'Логин',
-        unique=True,
-        null=False,
-        max_length=150,
-        validators=[
-            RegexValidator(
-                regex='^[\w.@+-]+\z',
-                message='Incorrect name'
-            )
-        ]
-    )
     email = models.EmailField(
-        'Электронная почта',
+        'Email',
         max_length=254,
         unique=True,
         null=False,
     ),
-    first_name = models.CharField(
-        'Имя',
-        max_length=150,
-        blank=True,
-    ),
-    last_name = models.CharField(
-        'Фамилия',
-        max_length=150,
-        blank=True,
-    ),
     bio = models.TextField(
-        'Биография',
+        'Biography',
         blank=True,
     )
     role = models.CharField(
-        'Роль',
+        'Role',
         choices=ROLE_CHOICES,
         default='USER'
     )
