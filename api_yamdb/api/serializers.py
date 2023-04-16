15390f409d8f89a77b1c605071c4631c4553cd35
Иван Березный <iberenznyi@yandex.ru>
2023-04-17 00:31:50 +0300

    Issues 9: model_feedback_is_done_mark5_api+

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from django.core.exceptions import ValidationError
+from django.shortcuts import get_object_or_404
+
+from reviews.models import Review
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    title = serializers.SlugRelatedField(
+        slug_field='name',
+        read_only=True
+    )
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
+
+    def validate_score(self, value):
+        if 0 > value > 10:
+            raise serializers.ValidationError('Rating from 1 to 10!')
+        return value
+
+    def validate(self, data):
+        request = self.context['request']
+        author = request.user
+        title_id = self.context.get('view').kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
+        if (
+            request.method == 'POST'
+            and Review.objects.filter(title=title, author=author).exists()
+        ):
+            raise ValidationError('You can only leave one '
+                                  'review for a product!')
+        return data
+
+    class Meta:
+        fields = '__all__'
+        model = Review
---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,17 @@
+from django.urls import include, path
+from rest_framework.routers import SimpleRouter
+
+from .views import (ReviewViewSet)
+
+app_name = 'api'
+
+router = SimpleRouter()
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews'
+)
+
+urlpatterns = [
+    path('v1/', include(router.urls)),
+]
