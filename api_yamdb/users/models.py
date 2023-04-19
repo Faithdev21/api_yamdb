1f0622364289baf59aa710b15998a27f67ae07c3
Егор Лоскутов <egorkaloskutov@yandex.ru>
2023-04-19 17:38:03 +0300

    Add roles

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
 ROLE_CHOICES = [
     ('USER', 'user'),
     ('MODERATOR', 'moderator'),
@@ -23,3 +22,15 @@ class User(AbstractUser):
 
     def __str__(self) -> str:
         return self.username
+
+    @property
+    def is_moderator(self):
+        return self.role == 'MODERATOR'
+
+    @property
+    def is_admin(self):
+        return self.role == (
+                'ADMIN'
+                or self.is_staff
+                or self.is_superuser
+        )
